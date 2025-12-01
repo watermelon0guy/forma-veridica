8dd8743202e49ebc2fb2483949f4a2ff62764651
Mikhail Babichev <watermelon0guy@proton.me>
2025-12-01 20:58:33 +0300

    Draft new setup menu

--- reconstruction_app/src/app.rs
+++ reconstruction_app/src/app.rs
@@ -49,8 +49,8 @@ struct VideoData {
 enum PipelineState {
     #[default]
     FolderSetup,
-    CalibrationSetup,
-    VideoSetup,
+    FetchProject,
+    SetupMenu,
     ReadyToProcess,
 }
 
@@ -68,8 +68,8 @@ impl ReconstructionApp {
     fn render_content(&mut self, ctx: &egui::Context) {
         egui::CentralPanel::default().show(ctx, |ui| match self.pipeline_state {
             PipelineState::FolderSetup => self.render_folder_setup(ui),
-            PipelineState::CalibrationSetup => self.render_calibration_setup(ui),
-            PipelineState::VideoSetup => todo!(),
+            PipelineState::FetchProject => self.fetch_project(ui),
+            PipelineState::SetupMenu => self.render_setup_menu(ui),
             PipelineState::ReadyToProcess => todo!(),
         });
     }
@@ -94,7 +94,7 @@ impl ReconstructionApp {
                             calibration_data: None,
                             video_data: None,
                         };
-                        self.pipeline_state = PipelineState::CalibrationSetup // Переходим к следующему этапу
+                        self.pipeline_state = PipelineState::FetchProject // Переходим к следующему этапу
                     }
                     None => return,
                 }
@@ -102,40 +102,87 @@ impl ReconstructionApp {
         });
     }
 
-    fn render_calibration_setup(&mut self, ui: &mut egui::Ui) {
+    fn render_setup_menu(&mut self, ui: &mut egui::Ui) {
+        ui.label(egui::RichText::new(format!(
+            "Путь проекта теперь установлен в {}",
+            self.resources.project_path.as_ref().unwrap().display()
+        )));
+        ui.columns(2, |columns| {
+            self.render_camera_parameters_setup(&mut columns[0]);
+            self.render_video_setup(&mut columns[1]);
+        });
+    }
+
+    fn render_camera_parameters_setup(&mut self, ui: &mut egui::Ui) {
         ui.vertical_centered(|ui| {
-            ui.label(egui::RichText::new(format!(
-                "Путь проекта теперь установлен в {}",
-                self.resources.project_path.as_ref().unwrap().display()
-            )));
-            ui.label(egui::RichText::new("Выберите файл с параметр камер").size(18.0));
-            let button = egui::Button::new(egui::RichText::new("Выбрать").size(18.0))
-                .min_size(egui::vec2(140.0, 40.0));
+            ui.heading("Параметры камеры");
 
-            if ui.add(button).clicked() {
-                match rfd::FileDialog::new()
-                    .set_title("Выбрать файл параметров")
-                    .pick_file()
-                {
-                    Some(p) => {
-                        let project_path = &self.resources.project_path.as_ref().unwrap();
-                        let dest_path = project_path.join(p.file_name().unwrap());
-                        if let Err(_) = std::fs::copy(&p, &dest_path) {
-                            return;
-                        }
+            match &self.resources.calibration_data {
+                None => {
+                    ui.label(egui::RichText::new("Выберите файл с параметр камер"));
+                    let button = egui::Button::new(egui::RichText::new("Выбрать").size(18.0))
+                        .min_size(egui::vec2(140.0, 40.0));
 
-                        let cam_params = match load_camera_parameters(dest_path.to_str().unwrap()) {
-                            Ok(c) => c,
-                            Err(_) => return,
-                        };
-                        self.resources.calibration_data =
-                            Some(CalibrationData::new(dest_path, cam_params));
-
-                        self.pipeline_state = PipelineState::VideoSetup // Переходим к следующему этапу
+                    if ui.add(button).clicked() {
+                        self.pick_camera_parameters_file();
                     }
-                    None => return,
+                }
+                Some(calib_data) => {
+                    let num_cam = calib_data.num_cameras;
+                    ui.label(format!("В параметрах найдено {num_cam}"));
+                    let button =
+                        egui::Button::new(egui::RichText::new("Изменить параметры").size(18.0))
+                            .min_size(egui::vec2(140.0, 40.0));
+                    if ui.add(button).clicked() {
+                        self.pick_camera_parameters_file();
+                    }
                 }
             }
         });
+    }
+
+    fn pick_camera_parameters_file(&mut self) {
+        match rfd::FileDialog::new()
+            .set_title("Выбрать файл параметров")
+            .pick_file()
+        {
+            Some(file_path) => {
+                let project_path = self.resources.project_path.as_ref().unwrap();
+                let dest_path = project_path.join("camera_parameters.yml");
+
+                if let Err(_) = std::fs::copy(&file_path, &dest_path) {
+                    return;
+                }
+
+                let cam_params = match load_camera_parameters(dest_path.to_str().unwrap()) {
+                    Ok(c) => c,
+                    Err(_) => return,
+                };
+                self.resources.calibration_data = Some(CalibrationData::new(dest_path, cam_params));
+            }
+            None => return,
+        }
+    }
+
+    fn render_video_setup(&mut self, ui: &mut egui::Ui) {
+        ui.vertical_centered(|ui| ui.heading("Видео для анализа"));
+    }
+
+    fn fetch_camera_params(&mut self) {
+        let project_path = self.resources.project_path.as_ref().unwrap();
+        let file_path = project_path.join("camera_parameters.yml");
+
+        if file_path.exists() {
+            let cam_params = match load_camera_parameters(file_path.to_str().unwrap()) {
+                Ok(c) => c,
+                Err(_) => return,
+            };
+            self.resources.calibration_data = Some(CalibrationData::new(file_path, cam_params));
+        }
+    }
+
+    fn fetch_project(&mut self, ui: &mut egui::Ui) {
+        self.fetch_camera_params();
+        self.pipeline_state = PipelineState::SetupMenu;
     }
 }
